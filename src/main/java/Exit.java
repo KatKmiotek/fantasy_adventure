3ffb444540142abc88cdde50e277deae79b95705
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-07-23 15:01:47 +0100

    start of extensions

---
+++ src/main/java/Exit.java
@@ -0,0 +1,12 @@
+public enum Exit {
+    WEST(1),
+    EAST(2),
+    NORTH(3),
+    SOUTH(4);
+    private final int number;
+
+    Exit(int number) {
+        this.number = number;
+    }
+
+}
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Room {
+    private ArrayList<Exit> exits;
+
+    public Room() {
+        this.exits = new ArrayList<Exit>();
+    }
+
+    public ArrayList<Exit> getExits() {
+        return exits;
+    }
+
+    public void setExits(ArrayList<Exit> exits) {
+        this.exits = exits;
+    }
+
+//    public Exit randomExit(){
+//        ArrayList<Exit> shuffled = Collections.shuffle(exits);
+//        return shuffled.get(0);
+//    }
 }
